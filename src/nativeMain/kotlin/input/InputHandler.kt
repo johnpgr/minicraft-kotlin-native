7a6b241b0f29b2cb70af6cdf4bcd86a6f297f686
johnpgr <jp.greidinger@hotmail.com>
2025-06-26 22:41:05 -0300

    add sdl def for macos

--- src/nativeMain/kotlin/input/InputHandler.kt
+++ src/nativeMain/kotlin/input/InputHandler.kt
@@ -51,18 +51,15 @@ fun InputHandler.handleEvent(ev: SDL_Event) {
 
 fun InputHandler.toggle(keycode: SDL_Keycode, pressed: Boolean) {
     when (keycode) {
-        // Movimentação
         SDLK_KP_8, SDLK_W, SDLK_UP -> up.toggle(pressed)
         SDLK_KP_2, SDLK_S, SDLK_DOWN -> down.toggle(pressed)
         SDLK_KP_4, SDLK_A, SDLK_LEFT -> left.toggle(pressed)
         SDLK_KP_6, SDLK_D, SDLK_RIGHT -> right.toggle(pressed)
 
-        // Menu
         SDLK_TAB, SDLK_LALT, SDLK_RALT, SDLK_MODE, SDLK_RETURN, SDLK_X -> menu.toggle(
             pressed
         )
 
-        // Ataque
         SDLK_SPACE, SDLK_LCTRL, SDLK_RCTRL, SDLK_KP_0, SDLK_INSERT, SDLK_C -> attack.toggle(
             pressed
         )
